242370e23944764405cf1e4a89fd4aacbf538f5b
Luciano Senger <ljsenger@gmail.com>
2019-10-06 18:19:43 -0300

    Uso de variáveis com templates

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, json, url_for, redirect
 
+modelos={"0": "pegasus", "1": "vintage", "2": "sport"}
+
 app = Flask("app")
 
 @app.route("/")
@@ -9,6 +11,10 @@ def index():
 @app.route("/user/<name>")
 def user(name):
     return render_template("index.html", name=name)
+
+@app.route("/modelos")
+def produtos():
+    return render_template("produtos.html", m=modelos)
 
 @app.route('/favicon.ico')
 def favicon():
